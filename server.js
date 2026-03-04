802d1d9a16c6e19a35d20d7541d30c0f26e83283
roguestudios21 <atharv.choughule@gmail.com>
2026-03-04 18:40:19 +0530

    fix

--- server.js
+++ server.js
@@ -5,19 +5,19 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
 // Routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/privacy', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'privacy.html'));
+    res.sendFile(path.join(__dirname, 'privacy.html'));
 });
 
 app.get('/terms', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'terms.html'));
+    res.sendFile(path.join(__dirname, 'terms.html'));
 });
 
 app.listen(PORT, () => {
